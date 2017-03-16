0070bd6b01d827dcd53d2a2bb318a96282cea484
Aaron Doubek-Kraft <adoubekk@ucsc.edu>
2017-03-15 23:04:41 -0700

    Added normalization of undergrad and grad gpas

--- datasets/process.py
+++ datasets/process.py
@@ -29,6 +29,7 @@ def clean(string):
                 result = result + val + "|"
     return result[:-1]
 
+
 #try to open file in commmand line agruments
 filename1 = sys.argv[1]
 filename2 = sys.argv[2]
@@ -63,7 +64,18 @@ for row in data2:
 admitted = set(admitted_list)
 ID_index = fields.index("ID")
 RI_index = fields.index("Research Interests")
-temp = ""
+UG_GPA_index = fields.index("UG GPA")
+UG_SCALE_index = fields.index("UG GPA SCALE")
+GRAD_GPA_index = fields.index("GRAD GPA") + 1
+GRAD_SCALE_index = fields.index("GRAD GPA SCALE") + 1
+
+fields.insert(UG_SCALE_index + 1,"UG GPA (Normalized)")
+fields.insert(GRAD_SCALE_index + 1,"GRAD GPA (Normalized)")
+
+GPA = 0
+Scale = 1
+Result = 0
+exclusions = set({"","Other","Don't Know","None"})
 
 for row in data1:
     #add field indicating if student was admitted
@@ -74,7 +86,31 @@ for row in data1:
         
     #strip rankings from research interests
     row[RI_index] = clean(row[RI_index])
-        
+    
+    #normalize GPAs
+    
+    GPA = row[UG_GPA_index]
+    Scale = row[UG_SCALE_index]
+    if (GPA not in exclusions and Scale not in exclusions):
+        result = float(GPA)/float(Scale)
+        if (result <= 1):
+            row.insert(UG_SCALE_index + 1,result)
+        else:
+            row.insert(UG_SCALE_index + 1,"")
+    else:
+         row.insert(UG_SCALE_index + 1,"")
+         
+    GPA = row[GRAD_GPA_index]
+    Scale = row[GRAD_SCALE_index]
+    if (GPA not in exclusions and Scale not in exclusions):
+        result = float(GPA)/float(Scale)
+        if (result <=1):
+            row.insert(GRAD_SCALE_index + 1,result)
+        else:
+            row.insert(GRAD_SCALE_index + 1,"")
+    else:
+         row.insert(GRAD_SCALE_index + 1,"")
+         
 #Write to file
 out_filename = filename1.split('.')[0] #extract base name of input file
 out_file = open(out_filename+"_new.csv",'w',newline='')
